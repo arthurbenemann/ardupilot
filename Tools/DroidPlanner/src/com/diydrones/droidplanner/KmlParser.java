7031bbbf1fe0998231d051ee2e109493c58f7a29
arthurbenemann <arthur.benemann@gmail.com>
2013-01-17 15:04:39 -0200

    DroidPlanner: Extracted Kml parser class to a new file

---
+++ Tools/DroidPlanner/src/com/diydrones/droidplanner/KmlParser.java
@@ -0,0 +1,161 @@
+package com.diydrones.droidplanner;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.util.Log;
+import android.util.Xml;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Class to parse a Kml file, based on the code from
+ * http://developer.android.com/training/basics/network-ops/xml.html
+ * 
+ */
+public class KmlParser {
+	private final String ns = null;
+	
+	public class waypoint {
+		LatLng coord;
+		Double Height;
+
+		public waypoint(LatLng c, Double h) {
+			this.coord = c;
+			Height = h;
+		}
+
+		public waypoint(Double Lat, Double Lng, Double h) {
+			this.coord = new LatLng(Lat, Lng);
+			Height = h;
+		}
+	}
+
+	public List<waypoint> parse(InputStream in)
+			throws XmlPullParserException, IOException {
+		try {
+			XmlPullParser parser = Xml.newPullParser();
+			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,
+					false);
+			parser.setInput(in, null);
+			parser.nextTag();
+			return readFeed(parser);
+		} finally {
+			in.close();
+		}
+	}
+
+	private List<waypoint> readFeed(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
+		List<waypoint> entries = new ArrayList<waypoint>();
+
+		parser.require(XmlPullParser.START_TAG, ns, "kml");
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG) {
+				continue;
+			}
+			String name = parser.getName();
+			// Starts by looking for the entry tag
+			if (name.equals("Placemark")) {
+				entries.add(readPlacemark(parser));
+			} else {
+				skip(parser);
+			}
+		}
+		return entries;
+	}
+
+	private waypoint readPlacemark(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
+		parser.require(XmlPullParser.START_TAG, ns, "Placemark");
+		waypoint point = null;
+
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG) {
+				continue;
+			}
+			String name = parser.getName();
+			if (name.equals("Point")) {
+				point = readPoint(parser);
+			} else {
+				skip(parser);
+			}
+		}
+		return point;
+	}
+
+	// Processes title tags in the feed.
+	private waypoint readPoint(XmlPullParser parser) throws IOException,
+			XmlPullParserException {
+		waypoint point = null;
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG) {
+				continue;
+			}
+			String name = parser.getName();
+			if (name.equals("coordinates")) {
+				point = readCoordinate(parser);
+			} else {
+				skip(parser);
+			}
+		}
+		return point;
+	}
+
+	private waypoint readCoordinate(XmlPullParser parser)
+			throws IOException, XmlPullParserException {
+		Double Lat, Lng, h;
+		
+		
+		parser.require(XmlPullParser.START_TAG, ns, "coordinates");
+		String[] title = readText(parser).split(",");
+		parser.require(XmlPullParser.END_TAG, ns, "coordinates");
+		
+		Log.d("", "coord:" + title);
+		Lat = Double.valueOf(title[0]);
+		Lng = Double.valueOf(title[1]);
+		h = Double.valueOf(title[2]);
+		
+		return (new waypoint(Lat, Lng, h));
+	}
+
+	// For the tags title and summary, extracts their text values.
+	private String readText(XmlPullParser parser) throws IOException,
+			XmlPullParserException {
+		String result = "";
+		if (parser.next() == XmlPullParser.TEXT) {
+			result = parser.getText();
+			parser.nextTag();
+		}
+		return result;
+	}
+
+	// Skips tags the parser isn't interested in. Uses depth to handle
+	// nested tags. i.e.,
+	// if the next tag after a START_TAG isn't a matching END_TAG, it keeps
+	// going until it
+	// finds the matching END_TAG (as indicated by the value of "depth"
+	// being 0).
+	private void skip(XmlPullParser parser) throws XmlPullParserException,
+			IOException {
+		if (parser.getEventType() != XmlPullParser.START_TAG) {
+			throw new IllegalStateException();
+		}
+		int depth = 1;
+		while (depth != 0) {
+			switch (parser.next()) {
+			case XmlPullParser.END_TAG:
+				depth--;
+				break;
+			case XmlPullParser.START_TAG:
+				depth++;
+				break;
+			}
+		}
+	}
+}
