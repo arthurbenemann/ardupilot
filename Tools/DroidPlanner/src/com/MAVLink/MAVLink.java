341d30dd02673a975c96f57933327dcbad076870
Arthur Benemann <arthur.benemann@gmail.com>
2013-02-07 10:36:40 -0200

    DroidPlanner: Moving unpacking switch to messsage class

--- Tools/DroidPlanner/src/com/MAVLink/MAVLink.java
+++ Tools/DroidPlanner/src/com/MAVLink/MAVLink.java
@@ -1,13 +1,8 @@
 package com.MAVLink;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import android.util.Log;
 
 import com.MAVLink.Messages.MAVLinkMessage;
-import com.MAVLink.Messages.ardupilotmega.msg_attitude;
-import com.MAVLink.Messages.ardupilotmega.msg_heartbeat;
 
 public class MAVLink {
 
@@ -84,7 +79,7 @@ public class MAVLink {
 
 		case MAVLINK_PARSE_STATE_GOT_MSGID:
 			m.payload.add(c);
-			if (m.payload.size() == m.len) {
+			if (m.payloadIsFilled()) {
 				state = MAV_states.MAVLINK_PARSE_STATE_GOT_PAYLOAD;
 			}
 			break;
@@ -115,7 +110,7 @@ public class MAVLink {
 				}
 			} else { // Successfully received the message
 				try {
-					m = unpackMessage(m.payload);
+					m.unpackMessage();
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -131,38 +126,4 @@ public class MAVLink {
 		return m;
 	}
 
-	private MAVLinkMessage unpackMessage(List<Integer> payload) {
-		switch (m.msgid) {
-		case msg_heartbeat.MAVLINK_MSG_ID_HEARTBEAT:
-			Log.d("MAVLink", "HEARTBEAT");
-			return msg_heartbeat.unpack(payload);
-		case msg_attitude.MAVLINK_MSG_ID_ATTITUDE:
-			Log.d("MAVLink", "ATTITUDE");
-			return msg_attitude.unpack(payload);
-			/*
-			 * case msg_gps_raw.MAVLINK_MSG_ID_GPS_RAW: Log.d("MAVLink", "GPS");
-			 * return unpackGPS_RAW(); case
-			 * msg_sys_status.MAVLINK_MSG_ID_SYS_STATUS: Log.d("MAVLink",
-			 * "SYS_STATUS"); break; case
-			 * msg_waypoint_current.MAVLINK_MSG_ID_WAYPOINT_CURRENT:
-			 * Log.d("MAVLink", "WAYPOINT_CURRENT"); break; case
-			 * msg_global_position_int.MAVLINK_MSG_ID_GLOBAL_POSITION_INT:
-			 * Log.d("MAVLink", "GLOBAL_POSITION_INT"); break; case
-			 * msg_statustext.MAVLINK_MSG_ID_STATUSTEXT: Log.d("MAVLink",
-			 * "STATUSTEXT"); break; case msg_vfr_hud.MAVLINK_MSG_ID_VFR_HUD:
-			 * Log.d("MAVLink", "VRF_HUD"); break;
-			 */
-		default:
-			Log.d("MAVLink", "UNKNOW MESSAGE - " + m.msgid);
-			return null;
-		}
-	}
-
-	/*
-	 * private MAVLinkMessage unpackGPS_RAW() { msg_gps_raw m = new
-	 * msg_gps_raw(); m.usec = getInt64(0); m.fix_type = getInt8(8); m.lat =
-	 * getInt32(12); m.lon = getInt32(16); m.alt = getInt32(20); m.eph =
-	 * getInt16(24); m.epv = getInt16(26); m.v = getInt16(28); m. =
-	 * getInt16(28); return m; }
-	 */
 }
--- Tools/DroidPlanner/src/com/MAVLink/Messages/MAVLinkMessage.java
+++ Tools/DroidPlanner/src/com/MAVLink/Messages/MAVLinkMessage.java
@@ -2,6 +2,11 @@ package com.MAVLink.Messages;
 
 import java.util.List;
 
+import android.util.Log;
+
+import com.MAVLink.Messages.ardupilotmega.msg_attitude;
+import com.MAVLink.Messages.ardupilotmega.msg_heartbeat;
+
 
 public class MAVLinkMessage {
 	/**
@@ -55,4 +60,43 @@ public class MAVLinkMessage {
 		crc.finish_checksum(msgid);
 	}
 	
+	public MAVLinkMessage unpackMessage() {
+		switch (msgid) {
+		case msg_heartbeat.MAVLINK_MSG_ID_HEARTBEAT:
+			Log.d("MAVLink", "HEARTBEAT");
+			return msg_heartbeat.unpack(payload);
+		case msg_attitude.MAVLINK_MSG_ID_ATTITUDE:
+			Log.d("MAVLink", "ATTITUDE");
+			return msg_attitude.unpack(payload);
+			/*
+			 * case msg_gps_raw.MAVLINK_MSG_ID_GPS_RAW: Log.d("MAVLink", "GPS");
+			 * return unpackGPS_RAW(); case
+			 * msg_sys_status.MAVLINK_MSG_ID_SYS_STATUS: Log.d("MAVLink",
+			 * "SYS_STATUS"); break; case
+			 * msg_waypoint_current.MAVLINK_MSG_ID_WAYPOINT_CURRENT:
+			 * Log.d("MAVLink", "WAYPOINT_CURRENT"); break; case
+			 * msg_global_position_int.MAVLINK_MSG_ID_GLOBAL_POSITION_INT:
+			 * Log.d("MAVLink", "GLOBAL_POSITION_INT"); break; case
+			 * msg_statustext.MAVLINK_MSG_ID_STATUSTEXT: Log.d("MAVLink",
+			 * "STATUSTEXT"); break; case msg_vfr_hud.MAVLINK_MSG_ID_VFR_HUD:
+			 * Log.d("MAVLink", "VRF_HUD"); break;
+			 */
+		default:
+			Log.d("MAVLink", "UNKNOW MESSAGE - " + msgid);
+			return null;
+		}
+	}
+
+
+	public boolean payloadIsFilled() {
+		return (payload.size() == len);
+	}
+
+	/*
+	 * private MAVLinkMessage unpackGPS_RAW() { msg_gps_raw m = new
+	 * msg_gps_raw(); m.usec = getInt64(0); m.fix_type = getInt8(8); m.lat =
+	 * getInt32(12); m.lon = getInt32(16); m.alt = getInt32(20); m.eph =
+	 * getInt16(24); m.epv = getInt16(26); m.v = getInt16(28); m. =
+	 * getInt16(28); return m; }
+	 */
 }
