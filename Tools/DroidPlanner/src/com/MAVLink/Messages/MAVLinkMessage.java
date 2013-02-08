d3dbdfe71065e66d6ab9208df08e703a9a96a902
Arthur Benemann <arthur.benemann@gmail.com>
2013-02-08 10:07:14 -0200

    DroidPlanner: Deletes an old comented function.

--- Tools/DroidPlanner/src/com/MAVLink/Messages/MAVLinkMessage.java
+++ Tools/DroidPlanner/src/com/MAVLink/Messages/MAVLinkMessage.java
@@ -10,13 +10,5 @@ public class MAVLinkMessage {
 	public int compid;
 	public int msgid;
 	
-
-	/*
-	 * private MAVLinkMessage unpackGPS_RAW() { msg_gps_raw m = new
-	 * msg_gps_raw(); m.usec = getInt64(0); m.fix_type = getInt8(8); m.lat =
-	 * getInt32(12); m.lon = getInt32(16); m.alt = getInt32(20); m.eph =
-	 * getInt16(24); m.epv = getInt16(26); m.v = getInt16(28); m. =
-	 * getInt16(28); return m; }
-	 */
 }
 	
