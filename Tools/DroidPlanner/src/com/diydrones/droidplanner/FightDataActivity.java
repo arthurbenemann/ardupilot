5740507f01f78c6477e2bb0902f37e9cb4a83b4d
Arthur Benemann <arthur.benemann@gmail.com>
2013-02-14 11:31:40 -0200

    DroidPlanner: Add a map and MAVLink communication to flight data activity.

--- Tools/DroidPlanner/src/com/diydrones/droidplanner/FightDataActivity.java
+++ Tools/DroidPlanner/src/com/diydrones/droidplanner/FightDataActivity.java
@@ -4,76 +4,148 @@ import android.app.ActionBar;
 import android.app.ActionBar.OnNavigationListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
 
-public class FightDataActivity  extends android.support.v4.app.FragmentActivity
-	implements OnNavigationListener {
+import com.MAVLink.GPSMananger;
+import com.MAVLink.MAVLink;
+import com.MAVLink.Messages.MAVLinkMessage;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+
+public class FightDataActivity extends android.support.v4.app.FragmentActivity
+		implements OnNavigationListener {
+
+	private GoogleMap mMap;
+	private MenuItem connectButton;
+
+	MAVLink MAV = new MAVLink() {
+		@Override
+		public void onReceiveMessage(MAVLinkMessage msg) {
+			gpsManager.processMessage(msg);
+		}		
+		@Override
+		public void onDisconnect() {
+			connectButton.setTitle(getResources().getString(R.string.menu_connect));			
+		}		
+		@Override
+		public void onConnect() {
+			connectButton.setTitle(getResources().getString(R.string.menu_disconnect));
+		}
+	};
+	
+	GPSMananger gpsManager = new GPSMananger(MAV) {		
+		@Override
+		public void onGpsDataReceived(GPSdata data) {
+			Log.d("GPS", "LAT:"+data.position.coord.latitude+" LNG:"+data.position.coord.longitude+"ALT:"+data.position.Height+" heading:"+data.heading);			
+		}
+	};
 
 	
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-			// Set up the action bar to show a dropdown list.
-			ActionBar actionBar = getActionBar();
-			actionBar.setDisplayShowTitleEnabled(false);
-			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-			SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
-					R.array.menu_dropdown,
-					android.R.layout.simple_spinner_dropdown_item);
+		// Set up the action bar to show a dropdown list.
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
+				R.array.menu_dropdown,
+				android.R.layout.simple_spinner_dropdown_item);
 
-			actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
-			actionBar.setSelectedNavigationItem(2);
+		actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
+		actionBar.setSelectedNavigationItem(2);
 
-			setContentView(R.layout.flightdata);
-		}
+		setContentView(R.layout.flightdata);
 		
+		setUpMapIfNeeded();
+	}
 
-
-
-		@Override
-		public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-			switch (itemPosition) {
-			default:
-			case 0: // Planning
-				startActivity(new Intent(this, PlanningActivity.class));
-				return false;
-			case 1: // HUD
-				startActivity(new Intent(this, HUDActivity.class));
-				return false;
-			case 2: // Flight Data
-				//startActivity(new Intent(this, FightDataActivity.class));
-				return false;
-			case 3: // PID
-				startActivity(new Intent(this, PIDActivity.class));
-				return false;
-			case 4: // Terminal
-				startActivity(new Intent(this, TerminalActivity.class));
-				return false;
-			case 5: // GCP
-				startActivity(new Intent(this, GCPActivity.class));
-				return false;
-			}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
+	
+	
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		switch (itemPosition) {
+		default:
+		case 0: // Planning
+			startActivity(new Intent(this, PlanningActivity.class));
+			return false;
+		case 1: // HUD
+			startActivity(new Intent(this, HUDActivity.class));
+			return false;
+		case 2: // Flight Data
+			// startActivity(new Intent(this, FightDataActivity.class));
+			return false;
+		case 3: // PID
+			startActivity(new Intent(this, PIDActivity.class));
+			return false;
+		case 4: // Terminal
+			startActivity(new Intent(this, TerminalActivity.class));
+			return false;
+		case 5: // GCP
+			startActivity(new Intent(this, GCPActivity.class));
+			return false;
 		}
-		public boolean onCreateOptionsMenu(Menu menu) {
-			// Inflate the menu; this adds items to the action bar if it is present.
-			getMenuInflater().inflate(R.menu.menu_flightdata, menu);
+	}
 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_flightdata, menu);
+		connectButton = menu.findItem(R.id.menu_connect);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			byte[] buff = new byte[1];
+			buff[0] = '1';
+			MAV.sendBuffer(buff); // TODO only used for debbuging
 			return true;
+		case R.id.menu_connect:
+			MAV.toggleConnectionState();
+			return true;
+		default:
+			return super.onMenuItemSelected(featureId, item);
 		}
-		@Override
-		public boolean onMenuItemSelected(int featureId, MenuItem item) {
-			switch (item.getItemId()) {
-			case R.id.menu_settings:
-				return true;
-			case R.id.menu_connect:
-				return true;
-			default:
-				return super.onMenuItemSelected(featureId, item);
+	}
+	
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
 			}
 		}
+	}
+
+	private void setUpMap() {
+		mMap.setMyLocationEnabled(true);
+		mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+		
+		UiSettings mUiSettings = mMap.getUiSettings();
+		mUiSettings.setMyLocationButtonEnabled(true);
+		mUiSettings.setCompassEnabled(true);
+		mUiSettings.setTiltGesturesEnabled(false);
+		}
+	
+	
 
 }
